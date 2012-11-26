9fcff323056de7d83639e04032861954e804c0cb
waldrd15 <waldrd11@yahoo.com>
2012-11-26 09:03:33 -0500

    try to pull new spec tests

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,5 +1,6 @@
 class PaymentDetail < ActiveRecord::Base
-  attr_accessible :amount, :payable_id, :payable_type, :user_id
+
+  attr_accessible :amount, :payable_type, :payable_id, :user_id
 
   belongs_to :payable, polymorphic: true
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+
   attr_accessible :email, :name, :password_digest, :remember_token
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   has_many :payment_details
   has_many :boards
